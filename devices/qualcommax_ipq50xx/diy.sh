03252d04cbba92520d07926f8eb3bc39005f3a6e
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-12-21 22:32:18 +0800

    update

--- devices/qualcommax_ipq50xx/diy.sh
+++ devices/qualcommax_ipq50xx/diy.sh
@@ -8,3 +8,9 @@ rm -rf target/linux/qualcommax target/linux/generic package/firmware/ath11k-firm
 git_clone_path main https://github.com/georgemoussalem/openwrt-fork target/linux/qualcommax target/linux/generic package/firmware/ath11k-firmware package/firmware/ipq-wifi package/kernel/mac80211 package/kernel/qca-nss-dp package/kernel/qca-ssdk package/boot/uboot-envtools package/network/utils/iwinfo
 
 wget -N https://github.com/georgemoussalem/openwrt-fork/raw/refs/heads/main/include/kernel-6.6 -P include/
+
+git_clone_path master https://github.com/coolsnowwolf/lede mv target/linux/generic/hack-6.6
+rm -rf target/linux/generic/hack-6.6/929-Revert-genetlink*
+wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/generic/pending-6.6/613-netfilter_optional_tcp_window_check.patch -P target/linux/generic/pending-6.6/
+
+wget -N https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/16414.patch -P devices/common/patches/
